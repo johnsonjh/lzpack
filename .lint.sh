9c512d3f449e6f9e2246ab0daa55dc051b893e85
Jeffrey H. Johnson <johnsonjh.dev@gmail.com>
2026-05-26 06:22:29 -0400

    Update linting script
    Signed-off-by: Jeffrey H. Johnson <johnsonjh.dev@gmail.com>

--- .lint.sh
+++ .lint.sh
@@ -29,5 +29,17 @@ else
 fi
 make distclean > /dev/null 2>&1 || :
 
+printf '%s\n' ">> pvs-studio"
+rm -f compile_commands.json log.pvs 2> /dev/null
+rm -f -r ./pvsreport 2> /dev/null 2>&1
+env CC=clang bear -- scan-build make clean distclean all
+pvs-studio-analyzer analyze --intermodular -j 72 -o log.pvs
+if plog-converter -a "GA:1,2,3" -t fullhtml log.pvs \
+  -o pvsreport --indicateWarnings; then
+  :
+else
+  rc=1
+fi
+
 if [ "${rc}" = 0 ]; then printf '%s\n' ">> lint clean"; else printf '%s\n' ">> lint FAILED"; fi
 exit "${rc}"
