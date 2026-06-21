327858bac54367c39eb5db5b6a999c88bf6da7a6
Jeffrey H. Johnson <johnsonjh.dev@gmail.com>
2026-06-21 19:27:50 -0400

    Use SKIP_AUTO to skip compiler flag detection
    Signed-off-by: Jeffrey H. Johnson <johnsonjh.dev@gmail.com>

--- platform.mk
+++ platform.mk
@@ -14,7 +14,7 @@
 ################################################################################
 
 # Skip flag detection? (begin)
-ifdef PVS
+ifdef SKIP_AUTO
  SKIP_DETECTION:=1
 endif
 ifeq ($(MAKECMDGOALS),clean)
